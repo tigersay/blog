bf8fa19c4efa2bcfcad6c391eea90e7c086a1c14
Administrator <Administrator@ZGC--20110918VC>
2015-07-17 15:58:43 +0800

    - 加一些微博

--- js/daohang.js
+++ js/daohang.js
@@ -31,17 +31,19 @@ function createFooter(){
 function createWeiboMonthMenu(currMonth){
 	var menu_bar = document.getElementById("monthlist");
 	var content = "<div class=\"bannian\"><span id=\"seven\" class=\"month\"><a href=\"weibo_7.html\">2014年7月</a></span>" +
-			"<span id=\"eight\" class=\"month\"><a href=\"weibo_8.html\">2014年8月</a></span>" +
-			"<span id=\"nine\" class=\"month\"><a href=\"weibo_9.html\">2014年9月</a></span>" +
-			"<span id=\"ten\" class=\"month\"><a href=\"weibo_10.html\">2014年10月</a></span>" +
-			"<span id=\"eleven\" class=\"month\"><a href=\"weibo_11.html\">2014年11月</a></span>" +
-			"<span id=\"twelve\" class=\"month\"><a href=\"weibo_12.html\">2014年12月</a></span></div>" +
-			"<div class=\"bannian\"><span id=\"15_01\" class=\"month\"><a href=\"weibo_15_01.html\">2015年1月</a></span>" +
+	"<span id=\"eight\" class=\"month\"><a href=\"weibo_8.html\">2014年8月</a></span>" +
+	"<span id=\"nine\" class=\"month\"><a href=\"weibo_9.html\">2014年9月</a></span>" +
+	"<span id=\"ten\" class=\"month\"><a href=\"weibo_10.html\">2014年10月</a></span>" +
+	"<span id=\"eleven\" class=\"month\"><a href=\"weibo_11.html\">2014年11月</a></span>" +
+	"<span id=\"twelve\" class=\"month\"><a href=\"weibo_12.html\">2014年12月</a></span></div>" +
+	"<div class=\"bannian\">" + 
+	"<span id=\"15_01\" class=\"month\"><a href=\"weibo_15_01.html\">2015年1月</a></span>" +
 	"<span id=\"15_02\" class=\"month\"><a href=\"weibo_15_02.html\">2015年2月</a></span>"  + 
 	"<span id=\"15_03\" class=\"month\"><a href=\"weibo_15_03.html\">2015年3月</a></span>" +
 	"<span id=\"15_04\" class=\"month\"><a href=\"weibo_15_04.html\">2015年4月</a></span>" +
 	"<span id=\"15_05\" class=\"month\"><a href=\"weibo_15_05.html\">2015年5月</a></span>" + 
-	"<span id=\"15_06\" class=\"month\"><a href=\"weibo_15_06.html\">2015年6月</a></span>" + 
+	"<span id=\"15_06\" class=\"month\"><a href=\"weibo_15_06.html\">2015年6月</a></span></div>" + 
+	"<div class=\"bannian\">" + 
 	"<span id=\"15_07\" class=\"month\"><a href=\"weibo_15_07.html\">2015年7月</a></span>" ; 
 	
 	content = content + "</div>";
