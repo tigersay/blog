9f2758a73c27683ded422c93cdf5e7b83110255f
tiger <309650490@qq.com>
2019-03-04 15:42:12 +0800

    tiger

--- js/daohang.js
+++ js/daohang.js
@@ -95,7 +95,7 @@ function createWeiboMonthMenu(currMonth){
 	"<span id=\"18_12\" class=\"month\"><a href=\"weibo_18_12.html\">2018年12月</a></span></div>" +
 	"<div class=\"bannian\">" +
 	"<span id=\"19_01\" class=\"month\"><a href=\"weibo_19_01.html\">2019年1月</a></span>"  +
-	"<span id=\"19_02\" class=\"month\"><a href=\"weibo_19_02.html\">2019年2月</a></span>" +
+	"<span id=\"19_02\" class=\"month\"><a href=\"weibo_19_02.html\">2019年2月</a></span>"  +
 	"<span id=\"19_03\" class=\"month\"><a href=\"weibo_19_03.html\">2019年3月</a></span>" 
 	;
 	
